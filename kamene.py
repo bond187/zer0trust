b628ed1dfc3384cd235f9c57856bb1eb3970c7fc
bond187 <bbond2980@gmail.com>
2023-06-07 09:50:26 -0700

    remove experimental files

--- kamene.py
+++
@@ -1,6 +0,0 @@
-from kamene import *
-
-with PcapReader('Zer0Trust-5G.pcap-01.cap') as pcap_reader:
-  for pkt in pcap_reader:
-    #do something with the packet
-    print(pkt)
--- rdpcap.py
+++
@@ -1,8 +0,0 @@
-from scapy import *
-
-packets = rdpcap("Zer0Trust-5G.pcap-01.cap")
-packets.summary()
-
-for packet in packets:
-  if packet.hadlayer(UDP):
-    print(packet.summary())
