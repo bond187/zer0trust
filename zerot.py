f2909f0a01ad0c440cca508675f67241911e7e0f
bond187 <bbond2980@gmail.com>
2023-06-14 09:11:39 -0700

    add sample

--- zerot.py
+++ zerot.py
@@ -4,13 +4,13 @@ from stix2 import *
 import json
 
 def packet_list(read_file):
-'''
-packet_list() takes a file as an argument, and assuming that file is a csv holding pcap date (as exported by wireshark), and 
-outputs a dictionary of dictionaries. Each inner item corresponds to a packet in the packet capture, and the time, src, dst, 
-protocol, len, and info fields are identified and serve as keys for each packet item. In addition, to reduce massive duplication,
-lists of unique source ips and destination ips are returned. That way, when stix objects are being created later on, there will
-be network traffic for each packet, and one source ip and destination ip, which have relationships to many network traffic objects.
-'''
+  '''
+  packet_list() takes a file as an argument, and assuming that file is a csv holding pcap date (as exported by wireshark), and 
+  outputs a dictionary of dictionaries. Each inner item corresponds to a packet in the packet capture, and the time, src, dst, 
+  protocol, len, and info fields are identified and serve as keys for each packet item. In addition, to reduce massive duplication,
+  lists of unique source ips and destination ips are returned. That way, when stix objects are being created later on, there will
+  be network traffic for each packet, and one source mac and destination mac, which have relationships to many network traffic objects.
+  '''
   with open(read_file, 'r') as file:
     packets = dict()
     for i, line in enumerate(file.readlines(),0):
@@ -35,34 +35,34 @@ be network traffic for each packet, and one source ip and destination ip, which 
       if packets[pkt]["dst"] not in unique_dsts:
         unique_dsts.append(packets[pkt]["dst"])
 
-  print(len(unique_srcs), len(unique_dsts))
+  print("Unique sources: {}, Unique destinations: {}".format(len(unique_srcs), len(unique_dsts)))
   return packets, unique_srcs, unique_dsts
 
 def create_bundle(packets, un_srcs, un_dsts, file):
-'''
-create_bundle does what it says on the tin, it takes the dict of dicts object packets, unique sources and destination ip lists,
-and the name of the input file (to intelligently name the output file). It iterates over the packet info provided and makes
-a stix bundle of network traffic objects, ipv6 objects, and relationshipd between them. The 2G sample file ends up with 36k 
-objects, way too big for my stig to render, but the 5G file has only 1528, which is more managable.
-'''
+  '''
+  create_bundle does what it says on the tin, it takes the dict of dicts object packets, unique sources and destination ip lists,
+  and the name of the input file (to intelligently name the output file). It iterates over the packet info provided and makes
+  a stix bundle of network traffic objects, ipv6 objects, and relationshipd between them. The 2G sample file ends up with 36k 
+  objects, way too big for my stig to render, but the 5G file has only 1528, which is more managable.
+  '''
   bundle = Bundle()
-  print(bundle)
-  print(type(bundle))
+  #print(bundle)
+  #print(type(bundle))
 
   bundle = dict(bundle)
-  print(type(bundle))
+  #print(type(bundle))
   objects = list()
 
   src_ids = dict()
   dst_ids = dict()
 
   for src in un_srcs:
-    new_src = IPv6Address(value= src)
+    new_src = MACAddress(value= src)
     objects.append(json.loads(new_src.serialize()))
     src_ids[src] = new_src.id
 
   for dst in un_dsts:
-    new_dst = IPv6Address(value= dst)
+    new_dst = MACAddress(value= dst)
     objects.append(json.loads(new_dst.serialize()))
     dst_ids[dst] = new_dst.id
 
@@ -81,14 +81,14 @@ objects, way too big for my stig to render, but the 5G file has only 1528, which
                             target_ref=dst_ids[packets[key]["dst"]])
     objects.append(json.loads(dst_rel.serialize()))
 
-  print(len(objects))
+  #print(len(objects))
   bundle["objects"] = objects
   parse(bundle)
-  print(type(bundle))
+  #print(type(bundle))
 
   filename, ext = os.path.splitext(file)
 
-  with open("./{}-out.json".format(filename), "w") as w_file:
+  with open("{}-out.json".format(filename), "w") as w_file:
 
     json.dump(bundle, w_file, indent=4)
 
